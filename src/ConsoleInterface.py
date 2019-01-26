3cd30a743ab11e223259550e2cce9fc3f2077a2c
marcozeller <mzeller@ethz.ch>
2019-01-26 13:50:02 +0100

    first version of storing items in databases stands

--- src/ConsoleInterface.py
+++ src/ConsoleInterface.py
@@ -3,17 +3,56 @@
 """
 Module Docstring
 """
+import npyscreen as nps
+import DatabaseInterface as dbi
 
 __author__ = "Marco Zeller"
 __version__ = "0.0.0"
 __license__ = "MIT"
 
 
-def main():
-    """ Main entry point of the app """
-    pass
+class AddItem(nps.ActionFormV2):
+    def create(self):
+        self._name = self.add(nps.TitleText, name="name: ")
+        self._function = self.add(nps.TitleText, name="function: ")
+        self._weight = self.add(nps.TitleText, name="weight: ")
+        self._volume = self.add(nps.TitleText, name="volume: ")
+        self._price = self.add(nps.TitleText, name="price: ")
+        self._amount = self.add(nps.TitleText, name="amount: ")
+
+        self._name.value = "Test1"
+        self._function.value = "Function"
+        self._weight.value = "0"
+        self._volume.value = "0"
+        self._price.value = "0"
+        self._amount.value = "0"
+
+    def on_ok(self):
+        item_data = {}
+        item_data['name'] = self._name.value
+        item_data['function'] = self._function.value
+        item_data['weight'] = int(self._weight.value)
+        item_data['volume'] = int(self._volume.value)
+        item_data['price'] = int(self._price.value)
+        item_data['amount'] = int(self._amount.value)
+
+        self.parentApp.db.store_new_item_in_db(item_data)
+
+    def on_cancel(self):
+        pass
+
+    def afterEditing(self):
+        self.parentApp.setNextForm('MAIN')
+
+
+class App(nps.NPSAppManaged):
+    def onStart(self):
+        self.db = dbi.Database(':memory:')
+        self.add_item = self.addForm('MAIN',
+                                     AddItem,
+                                     name="Add a new Item")
 
 
 if __name__ == "__main__":
-    """ This is executed when run from the command line """
-    main()
+    """ TODO: """
+    app = App().run()
--- src/DatabaseInterface.py
+++ src/DatabaseInterface.py
@@ -3,10 +3,44 @@
 """
 TODO: Module Docstring
 """
+import sqlite3
 
-__author__ = "Marco Zeller"
-__version__ = "0.0.0"
-__license__ = "MIT"
+
+class Database:
+    def __init__(self, db_name):
+        self.db_name = db_name
+        self.conn = sqlite3.connect(db_name)
+        self.cursor = self.conn.cursor()
+        self.initialize_db()
+
+    def initialize_db(self):
+        """ Creates the tables needed for storing the data if
+            they do not already exist """
+
+        self.cursor.execute("""CREATE TABLE IF NOT EXISTS items(
+                               id integer primary key autoincrement,
+                               name text,
+                               function text,
+                               weight integer,
+                               volume integer,
+                               price integer,
+                               amount integer) """)
+
+    def store_new_item_in_db(self, item_values):
+        """ Stores a new item in the database.
+            item_values is a dict with keys and values
+            TODO: make this Docstring more explainatory"""
+        with self.conn:
+            item_values['id'] = None
+            self.cursor.execute("""INSERT INTO items VALUES
+                                   (:id,
+                                    :name,
+                                    :function,
+                                    :weight,
+                                    :volume,
+                                    :price,
+                                    :amount)""",
+                                item_values)
 
 
 if __name__ == "__main__":
