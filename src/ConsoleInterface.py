e89c3a8a4e05c5078d4c00cb99d41c7f20f643c8
marcozeller <mzeller@ethz.ch>
2019-01-26 12:27:44 +0100

    Added files with boilerplate code to define the general structure of the application

---
+++ src/ConsoleInterface.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Module Docstring
+"""
+
+__author__ = "Marco Zeller"
+__version__ = "0.0.0"
+__license__ = "MIT"
+
+
+def main():
+    """ Main entry point of the app """
+    pass
+
+
+if __name__ == "__main__":
+    """ This is executed when run from the command line """
+    main()
---
+++ src/DatabaseInterface.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+TODO: Module Docstring
+"""
+
+__author__ = "Marco Zeller"
+__version__ = "0.0.0"
+__license__ = "MIT"
+
+
+if __name__ == "__main__":
+    """ TODO: Execute some tests """
+    pass
