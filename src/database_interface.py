80a63c085b6b8e9fce46e383c86d1dc26c5d611f
marcozeller <mzeller@ethz.ch>
2019-02-02 11:54:25 +0100

    formatted docstrings to be a bit nicer to look at

--- src/database_interface.py
+++ src/database_interface.py
@@ -14,8 +14,10 @@ class Database:
         self.initialize_db()
 
     def initialize_db(self):
-        """ Creates the tables needed for storing the data if
-            they do not already exist """
+        """
+        Creates the tables needed for storing the data if
+        they do not already exist
+        """
         with self.conn:
             self.cursor.execute("""CREATE TABLE IF NOT EXISTS items(
                                    id integer primary key autoincrement,
@@ -27,9 +29,11 @@ class Database:
                                    amount integer) """)
 
     def store_new_item_in_db(self, item_values):
-        """ Stores a new item in the database.
-            item_values is a dict with keys and values
-            TODO: make this Docstring more explainatory"""
+        """
+        Stores a new item in the database.
+        item_values is a dict with keys and values
+        TODO: make this Docstring more explainatory
+        """
         with self.conn:
             item_values['id'] = None
             self.cursor.execute("""INSERT INTO items VALUES
@@ -44,5 +48,7 @@ class Database:
 
 
 if __name__ == "__main__":
-    """ TODO: Execute some tests """
+    """
+    TODO: Execute some tests
+    """
     pass
