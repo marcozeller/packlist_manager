1b5362cbcefb01b6303727d21fe0e92279bb5393
marcozeller <mzeller@ethz.ch>
2019-04-21 09:49:34 +0200

    added and improved comments for better documentation

--- src/console_interface.py
+++ src/console_interface.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Module Docstring
+Describes an interactive user interface running in the console
+using npyscreen a library on top of ncurses.
+This module uses the database interface module to interact with the database
+and the utility module for more complex operations.
 """
 import npyscreen as nps
 import database_interface as dbi
 
 __author__ = "Marco Zeller"
-__version__ = "0.0.0"
+__version__ = "0.0.1"
 __license__ = "MIT"
 
 db_name = ':memory:'
@@ -21,7 +24,17 @@ default_values_new_item = {'name': "Name",
 
 
 class AddItem(nps.ActionFormV2):
+    """
+    Screen containing a formular to enter the attributes of a new item.
+    It has an 'OK' button which saves the new item in the database and
+    a 'CANCEL' button to leave the formular without changing the database.
+    """
     def create(self):
+        """
+        Draws the formular with fields to enter the attributes.
+        Fills the field with default values.
+        """
+        # draw the fields needed to enter the attributes
         self._name = self.add(nps.TitleText, name="name: ")
         self._function = self.add(nps.TitleText, name="function: ")
         self._weight = self.add(nps.TitleText, name="weight: ")
@@ -29,6 +42,7 @@ class AddItem(nps.ActionFormV2):
         self._price = self.add(nps.TitleText, name="price: ")
         self._amount = self.add(nps.TitleText, name="amount: ")
 
+        # fill in the fields with the default values
         self._name.value = default_values_new_item['name']
         self._function.value = default_values_new_item['function']
         self._weight.value = default_values_new_item['weight']
@@ -37,6 +51,11 @@ class AddItem(nps.ActionFormV2):
         self._amount.value = default_values_new_item['amount']
 
     def on_ok(self):
+        """
+        Gets called when the 'OK' button is pressed.
+        Saves all the attributes into the database.
+        """
+        # create a dictionary containing the attributes of the new item
         item_data = {}
         item_data['name'] = self._name.value
         item_data['function'] = self._function.value
@@ -45,8 +64,10 @@ class AddItem(nps.ActionFormV2):
         item_data['price'] = int(self._price.value)
         item_data['amount'] = int(self._amount.value)
 
+        # send the dictionary to the database interface
         self.parentApp.db.store_new_item_in_db(item_data)
 
+        # reset to default entries for next time the formular it is used
         self._name.value = default_values_new_item['name']
         self._function.value = default_values_new_item['function']
         self._weight.value = default_values_new_item['weight']
@@ -55,20 +76,31 @@ class AddItem(nps.ActionFormV2):
         self._amount.value = default_values_new_item['amount']
 
     def on_cancel(self):
+        """
+        Gets called when the 'CANCEL' button is pressed.
+        Make no changes to the database and exit the formular.
+        """
         self.parentApp.setNextForm(None)
         pass
 
 
 class App(nps.NPSAppManaged):
     def onStart(self):
+        # add an abstract database object to the application
+        # used by user interface to make changes to the database
         self.db = dbi.Database(self.db_name)
+
+        # add the different screens to the application
+        # 'MAIN' is the starting screen
         self.add_item = self.addForm('MAIN',
                                      AddItem,
                                      name="Add a new Item")
 
 
 if __name__ == "__main__":
-    """ TODO: """
+    """
+    Runs the application and connects the user interface to the database.
+    """
     app = App()
     app.db_name = db_name
     app = app.run()
--- src/database_interface.py
+++ src/database_interface.py
@@ -1,22 +1,32 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-TODO: Module Docstring
+This module is used by the userinterfaces to interact with the database.
+It offers abstractions for all the functionality provided by the database.
 """
 import sqlite3
 
 
 class Database:
     def __init__(self, db_name):
+        """
+        The argument db_name is a string, giving the name of the database
+        in the filesystem.
+        Use db_name = ':memory:' to create a temporary database for testing.
+        """
         self.db_name = db_name
         self.conn = sqlite3.connect(db_name)
         self.cursor = self.conn.cursor()
+
+        # initialize the database here to take the burden off the user
         self.initialize_db()
 
     def initialize_db(self):
         """
         Creates the tables needed for storing the data if
-        they do not already exist
+        they do not already exist.
+        Needs be called before a new database can be used.
+        If called on an existing database it does not have any side effects.
         """
         with self.conn:
             self.cursor.execute("""CREATE TABLE IF NOT EXISTS items(
@@ -31,8 +41,12 @@ class Database:
     def store_new_item_in_db(self, item_values):
         """
         Stores a new item in the database.
-        item_values is a dict with keys and values
-        TODO: make this Docstring more explainatory
+        item_values is a dict with the attribute's name (String) as key
+        to the corresponding value.
+        Item's 'id' is an integer internally used for referring to an item
+        read from the database before.
+        If the item_values dict does not provide a value for the key 'id'
+        this function creates a new instance of this item in the database.
         """
         with self.conn:
             item_values['id'] = None
@@ -49,6 +63,8 @@ class Database:
 
 if __name__ == "__main__":
     """
-    TODO: Execute some tests
+    TODO: Execute some tests.
+    EDIT: Run: 'py.test -s' to run all the existing tests.
+    Might be changed in the future.
     """
     pass
