e402cc607886c46dce87a6115e77d4dd18f23f5e
marcozeller <mzeller@ethz.ch>
2019-05-01 21:42:42 +0200

    implemented a testcases for updating items via the console interface

--- src/test_console_interface.py
+++ src/test_console_interface.py
@@ -222,7 +222,8 @@ def test_list_items_after_inserting_single_item():
     # make sure the list is only one entry long by clicking down
     # until we are on the 'OK' button and hit it
     # TODO: This step needs to be revised - figure out how to do that...
-    #       At the moment only fails if to mainy entries are present
+    #       At the moment only fails if to many entries are present.
+    #       Need to figure out how to read from the screen.
     nps.TEST_SETTINGS['TEST_INPUT'] += [curses.KEY_DOWN]
     nps.TEST_SETTINGS['TEST_INPUT'] += [10]  # 10 is the code for the Enter Key
 
@@ -236,8 +237,8 @@ def test_list_items_after_inserting_single_item():
 
 def test_list_items_after_inserting_multiple_items():
     """
-    Inserts several items via the console-interface and check if there are
-    corresponding entries in the list items screen.
+    Inserts several items via the console-interface and check if there are a
+    corresponding number of entries in the list items screen.
     """
     for i in range(10):
         go_to_add_item_screen_from_main_menu()
@@ -250,7 +251,8 @@ def test_list_items_after_inserting_multiple_items():
 
     # make sure the list has the correct number of entries.
     # TODO: This step needs to be revised - figure out how to do that...
-    #       At the moment only fails if to mainy entries are present
+    #       At the moment only fails if to many entries are present.
+    #       Need to figure out how to read from the screen.
     nps.TEST_SETTINGS['TEST_INPUT'] += 10*[curses.KEY_DOWN]
     nps.TEST_SETTINGS['TEST_INPUT'] += [10]  # 10 is the code for the Enter Key
 
@@ -260,3 +262,145 @@ def test_list_items_after_inserting_multiple_items():
     app = coi.App()
     app.db_name = ':memory:'
     app.run(fork=False)  # needs to run "py.test -s" else does not work
+
+
+def change_item_values(from_item, to_item):
+    """
+    Change the items data by deleting values from_item
+    and replacing with the values found in to_item.
+    from_item and to_item are both dictionaries with values for the respective
+    attribute-string as key.
+    """
+    # delete the existing values by pressing backspace as many times as
+    # there are characters in from_values values
+    nps.TEST_SETTINGS['TEST_INPUT'] += [curses.KEY_BACKSPACE for char in
+                                        from_item['name']]
+    # then enter the item's corresponding value
+    nps.TEST_SETTINGS['TEST_INPUT'] += to_item['name']
+    # go to the next field and repeat
+    nps.TEST_SETTINGS['TEST_INPUT'] += [curses.KEY_DOWN]
+
+    nps.TEST_SETTINGS['TEST_INPUT'] += [curses.KEY_BACKSPACE for char in
+                                        from_item['function']]
+    nps.TEST_SETTINGS['TEST_INPUT'] += to_item['function']
+    nps.TEST_SETTINGS['TEST_INPUT'] += [curses.KEY_DOWN]
+
+    nps.TEST_SETTINGS['TEST_INPUT'] += [curses.KEY_BACKSPACE for char in
+                                        str(from_item['weight'])]
+    nps.TEST_SETTINGS['TEST_INPUT'] += str(to_item['weight'])
+    nps.TEST_SETTINGS['TEST_INPUT'] += [curses.KEY_DOWN]
+
+    nps.TEST_SETTINGS['TEST_INPUT'] += [curses.KEY_BACKSPACE for char in
+                                        str(from_item['volume'])]
+    nps.TEST_SETTINGS['TEST_INPUT'] += str(to_item['volume'])
+    nps.TEST_SETTINGS['TEST_INPUT'] += [curses.KEY_DOWN]
+
+    nps.TEST_SETTINGS['TEST_INPUT'] += [curses.KEY_BACKSPACE for char in
+                                        str(from_item['price'])]
+    nps.TEST_SETTINGS['TEST_INPUT'] += str(to_item['price'])
+    nps.TEST_SETTINGS['TEST_INPUT'] += [curses.KEY_DOWN]
+
+    nps.TEST_SETTINGS['TEST_INPUT'] += [curses.KEY_BACKSPACE for char in
+                                        str(from_item['amount'])]
+    nps.TEST_SETTINGS['TEST_INPUT'] += str(to_item['amount'])
+
+
+def test_update_items():
+    """
+    Inserts several items modify them via the console-interface
+    and check if there are corresponding entries in the list items screen.
+    """
+    for i in range(10):
+        go_to_add_item_screen_from_main_menu()
+
+        insert_item_values_in_fields(test_item)
+
+        press_ok_button_after_entering_values_in_add_item_screen()
+
+    go_to_list_item_screen_from_main_menu()
+
+    new_item = {'name': "NewName",
+                'function': "NewTestFunction",
+                'weight': 2,
+                'volume': 3,
+                'price': 4,
+                'amount': 5}
+
+    for i in range(10):
+        # select first item to change
+        nps.TEST_SETTINGS['TEST_INPUT'] += [10]  # 10 is the code for the Enter Key
+        change_item_values(test_item, new_item)
+        press_ok_button_after_entering_values_in_add_item_screen()
+
+        # put cursor on next item
+        nps.TEST_SETTINGS['TEST_INPUT'] += [curses.KEY_DOWN]
+
+    # for the last loop iteration above sets the cursor on the 'OK' button
+    nps.TEST_SETTINGS['TEST_INPUT'] += [10]  # 10 is the code for the Enter Key
+
+    press_ok_button_from_main_menu()
+
+    # run the test-input on the application
+    app = coi.App()
+    app.db_name = ':memory:'
+    app.run(fork=False)  # needs to run "py.test -s" else does not work
+
+    # manually look into database to see if items have been changed
+    with app.db.conn:
+        app.db.cursor.execute(""" SELECT * FROM items """)
+        item_list = app.db.cursor.fetchall()
+    assert len(item_list) == 10
+    for i in range(10):
+        assert not tdi.compare_item_data(item_list[i], test_item)
+        assert tdi.compare_item_data(item_list[i], new_item)
+
+
+def test_update_items_cancel_button():
+    """
+    Inserts several items attempt to change their values
+    in the change item screen, but then presses the 'CANCEL' button.
+    Then check that none of the entries have been modified.
+    """
+    for i in range(10):
+        go_to_add_item_screen_from_main_menu()
+
+        insert_item_values_in_fields(test_item)
+
+        press_ok_button_after_entering_values_in_add_item_screen()
+
+    go_to_list_item_screen_from_main_menu()
+
+    new_item = {'name': "NewName",
+                'function': "NewTestFunction",
+                'weight': 2,
+                'volume': 3,
+                'price': 4,
+                'amount': 5}
+
+    for i in range(10):
+        # select first item to change
+        nps.TEST_SETTINGS['TEST_INPUT'] += [10]  # 10 is the code for the Enter Key
+        change_item_values(test_item, new_item)
+        press_cancel_button_after_entering_values_in_add_item_screen()
+
+        # put cursor on next item
+        nps.TEST_SETTINGS['TEST_INPUT'] += [curses.KEY_DOWN]
+
+    # for the last loop iteration above sets the cursor on the 'OK' button
+    nps.TEST_SETTINGS['TEST_INPUT'] += [10]  # 10 is the code for the Enter Key
+
+    press_ok_button_from_main_menu()
+
+    # run the test-input on the application
+    app = coi.App()
+    app.db_name = ':memory:'
+    app.run(fork=False)  # needs to run "py.test -s" else does not work
+
+    # manually look into database to see if items have not been changed
+    with app.db.conn:
+        app.db.cursor.execute(""" SELECT * FROM items """)
+        item_list = app.db.cursor.fetchall()
+    assert len(item_list) == 10
+    for i in range(10):
+        assert tdi.compare_item_data(item_list[i], test_item)
+        assert not tdi.compare_item_data(item_list[i], new_item)
